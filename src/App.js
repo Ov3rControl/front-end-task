87a1a7bc1fa4326acf2386fe3b6fb53d5b1bdb02
Mohamed Adel <ov3rcontrol@Mohameds-iMac.local>
2020-03-12 11:48:38 +0200

    :sparkles: Add Hero Section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Header from './components/layout/Header';
+import Hero from './components/layout/Hero';
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/layout/Hero.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Jumbotron } from 'react-bootstrap';
+function Hero() {
+  return (
+    <Jumbotron fluid style={divStyle}>
+      <div style={mainContainer}>
+        <h1 style={mainCaption}>RISEUP SUMMIT 2020</h1>
+        <h1 style={hashtagCaption}>#JOURNEYTOGROWTH</h1>
+      </div>
+    </Jumbotron>
+  );
+}
+const divStyle = {
+  height: 800,
+  backgroundImage: `url('https://riseupsummit.com/static/media/HomepageHeader19blue.414fe687.jpg')`
+};
+const mainCaption = {
+  color: 'white',
+  fontSize: 50,
+  fontWeight: 800
+};
+const hashtagCaption = {
+  color: 'white',
+  fontSize: 30,
+  fontWeight: 800
+};
+const mainContainer = {
+  margin: 30
+};
+export default Hero;
